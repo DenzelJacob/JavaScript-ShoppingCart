c1c33487f2a29b1f0f603ac5e2e10c509d7c8b04
Denzel Jacob <70809285+DenzelJacob@users.noreply.github.com>
2022-08-02 08:38:19 -0400

    Cleaning and Comments
    Cleaned up code and added comments for clarity

--- JavaScript-ShoppingCart/index.js
+++ JavaScript-ShoppingCart/index.js
@@ -1,15 +1,19 @@
 console.clear();
+
 let shopCartArr = [];
-//localStorage.removeItem("dj-cartList");
+
+// retrieves previous session data from local storage if exists
 if (JSON.parse(localStorage.getItem("dj-cartList")) !== null) {
   shopCartArr = JSON.parse(localStorage.getItem("dj-cartList"));
 }
 
+
+//sets up intial totals and quantities
 changeAmount();
-//console.log("test complete");
+
+//creates the cart
 initCart(shopCartArr);
-//console.log("test");
-//console.log(shopCartArr);
+
 
 class shopItem {
   constructor(name, price) {
@@ -25,9 +29,13 @@ class shopItem {
   }
 }
 
+
+// event listeners for click on "add to cart" or "remove" and change on specific item quantity
 document.addEventListener("click", clickHandler);
 document.addEventListener("change", updateValue);
 
+
+// gets the id for the item once the item quantity is changed and passes it to the update function
 function updateValue(e) {
   shopItem.quantity = e.target.value;
 
@@ -40,20 +48,26 @@ function updateValue(e) {
   updatesubtotal(e.target.dataset.id);
 }
 
+
+// event listener handler, than controls if the click was an add or remove click
 function clickHandler(e) {
   const tar = e.target;
   if (tar.matches("button")) {
     if (tar.innerText === "Add to cart") {
-      //console.log(tar.dataset.id);
+      // add to cart is clicked
+
+
       addItemCart(tar.dataset.id, tar.dataset.price);
-      // add
+      
     } else if (tar.innerText === "Remove") {
-      //remove
+      //remove is clicked
       remove(tar.dataset.id);
     }
   }
 }
 
+
+//helper function to check if item is in the cart
 function inCart(name) {
   let state = false;
   shopCartArr.forEach((e) => {
@@ -65,19 +79,22 @@ function inCart(name) {
   return state;
 }
 
+
+// add a item to the cart if item is not present once "add to cart" is pressed
 function addItemCart(id, price) {
+
   if (!inCart(id)) {
     let temp = new shopItem(id, price);
 
     createHtml(temp);
 
     shopCartArr.push(temp);
-  } else {
-    // make add for add cart
-  }
+  } 
   changeAmount();
 }
 
+
+// remove JS element once remove button is clicked
 function remove(id) {
   let tempCart = [];
   shopCartArr.forEach((e) => {
@@ -91,8 +108,12 @@ function remove(id) {
   changeAmount();
 }
 
+
+// dynamically create each JS item
 function createHtml(shopItem) {
-  const baseEle = document.querySelector(".details");
+// create each component of the element and append them to each other to create a completed JS element
+
+  const baseEle = document.querySelector(".details"); 
 
   let makeDiv = document.createElement("div");
   makeDiv.classList.add("cartItem");
@@ -124,10 +145,13 @@ function createHtml(shopItem) {
   makeButton.dataset.id = shopItem.name;
   makeDiv.appendChild(makeButton);
 
-  baseEle.appendChild(makeDiv);
+  baseEle.appendChild(makeDiv); // main element
 }
 
+
+//calculates subtotals for each item based on id passed
 function updatesubtotal(id) {
+
   document.getElementById("");
   let tempItem = document.querySelector(`.cartItem[data-id="${id}"]`);
   let tempPrice = 0;
@@ -143,8 +167,11 @@ function updatesubtotal(id) {
   changeAmount();
 }
 
+
+//iterates through the cart and keep track of quantities and prices, overall total
 function changeAmount() {
-  let numItems = 0; //works
+
+  let numItems = 0; 
   let totalBal = 0;
 
   shopCartArr.forEach((e) => {
@@ -159,24 +186,20 @@ function changeAmount() {
   saveCart();
 }
 
+
+// turns cart in to a json and saves it to local storage
 function saveCart() {
-  /// push to local storage
 
   let localCart = JSON.stringify(shopCartArr);
-  //console.log(localCart);
   localStorage.setItem("dj-cartList", localCart);
 }
 
+
+// initates cart by passing local storage
 function initCart(arr) {
+
   arr.forEach((e) => {
     createHtml(e);
     console.log(e.name);
-
-    //let tempItem = document.querySelector(`.cartItem[data-id="${e.name}"]`);
-
-    //tempItem.input.setAttribute("value", parseInt(e.quantity, 0));
-
-    //console.log(parseInt(e.quantity));
-    //console.log(tempItem);
   });
 }
